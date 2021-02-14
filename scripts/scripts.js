216c318bea919e69c7cfece18ded4e5c597a4ddb
Vlad <mellnote71@gmail.com>
2021-02-14 23:19:14 +0200

    drop animation not ended + click system was done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,13 +4,31 @@ let mainCoockieObj = document.getElementById('main_coockie');
 let coockieCountText = document.getElementById('coockiesCount');
 
 const userInfo = {
- coockiesCount : 0
+    coockiesCount: 0
 };
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function coockieDropAniation() {
+    let newCoockieElement = document.createElement('div');
+    newCoockieElement.style.right = getRandomInt(2000) + "px";
+    newCoockieElement.className = "newCoockie";
+    newCoockieElement.id = "tempCoockie"
+    document.body.append(newCoockieElement);
+}
 
 function coockieClick() {
     userInfo.coockiesCount++;
     coockieCountText.textContent = userInfo.coockiesCount;
-    console.log(userInfo.coockiesCount);
+    coockieDropAniation();
+
 }
 
- mainCoockieObj.onclick = coockieClick;
+function coockieAnimationDropCheck() {
+    let elem = document.getElementById('tempCoockie');
+    elem.addEventListener("transitionend" , elem.remove());
+}
+
+mainCoockieObj.onclick = coockieClick; // Coockie click function call 
