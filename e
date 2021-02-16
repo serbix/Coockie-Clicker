c9064cb4edf4e456c7f61df30e483c2d66a91e1d
Vlad <mellnote71@gmail.com>
2021-02-16 11:19:12 +0200

    1. Click system. 2.Purchase error animation

---
+++ e
@@ -0,0 +1,23 @@
+[33mcommit 2b268a4979a6e703236a52d61b0a506959d2cb74[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Vlad <mellnote71@gmail.com>
+Date:   Tue Feb 16 09:12:01 2021 +0200
+
+    click animation was done
+
+[33mcommit 216c318bea919e69c7cfece18ded4e5c597a4ddb[m
+Author: Vlad <mellnote71@gmail.com>
+Date:   Sun Feb 14 23:19:14 2021 +0200
+
+    drop animation not ended + click system was done
+
+[33mcommit 0c0c055380680f1a624756ce8922506f201e7ce5[m
+Author: Vlad <mellnote71@gmail.com>
+Date:   Sun Feb 14 21:43:50 2021 +0200
+
+    main function - click . Main parts of interface
+
+[33mcommit ebf796549a795a3ea07646610691b417775abb71[m
+Author: Vlad <mellnote71@gmail.com>
+Date:   Sat Feb 13 12:39:58 2021 +0200
+
+    init commit
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,17 @@
 "use strict";
 
 let mainCoockieObj = document.getElementById('main_coockie');
+let clickPowerUp = document.getElementById('clickPowerUp');
 let coockieCountText = document.getElementById('coockiesCount');
 
 const userInfo = {
-    coockiesCount: 0
+    coockiesCount: 0,
+    clickPower: 1
 };
+
+const upgradesCost = {
+    clickPowerCost: 50
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -16,17 +22,38 @@ function coockieDropAniation() {
     newCoockieElement.style.right = getRandomInt(2000) + "px";
     newCoockieElement.className = "newCoockie";
     document.body.append(newCoockieElement);
-    newCoockieElement.addEventListener("animationend" ,newCoockieElement.remove);
+    newCoockieElement.addEventListener("animationend", newCoockieElement.remove);
 }
 
 function coockieClick() {
-    userInfo.coockiesCount++;
+    userInfo.coockiesCount += userInfo.clickPower;
     coockieCountText.textContent = userInfo.coockiesCount;
     coockieDropAniation();
 
 }
 
+function clickPowerUpgrade() {
+    if (userInfo.coockiesCount >= upgradesCost.clickPowerCost) {
+        userInfo.coockiesCount -= upgradesCost.clickPowerCost
+        coockieCountText.textContent = userInfo.coockiesCount;
+        upgradesCost.clickPowerCost *= 2;
+        userInfo.clickPower++;
+       let elem =  document.getElementById('clickPowerCostId');
+       elem.textContent = upgradesCost.clickPowerCost;
+    } else {
+        CreatePurchaseError();
+        return;
+    }
+}
 
+function CreatePurchaseError() {
+    let errorMessage = document.createElement('div');
+    errorMessage.className = "errorMessage";
+    errorMessage.innerHTML = "<p class = infoBar >Not enough money</p>"
+    document.body.append(errorMessage);
+    errorMessage.addEventListener("animationend", errorMessage.remove);
+}
 
 
 mainCoockieObj.onclick = coockieClick; // Coockie click function call 
+clickPowerUp.onclick = clickPowerUpgrade; // Click power will increase on 1 
